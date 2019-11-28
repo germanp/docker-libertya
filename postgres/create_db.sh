1a1c8941746a31e4b4564a9ddf99ed8a8901d4d4
German Podesta <german.m.podesta@gmail.com>
2019-11-28 11:08:23 -0300

    Carga inicial.

---
+++ postgres/create_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+POSTGRES="psql --username ${POSTGRES_USER}"
+
+echo "Creating database: ${DB_NAME}"
+
+$POSTGRES <<EOSQL
+CREATE DATABASE ${DB_NAME} WITH ENCODING='UTF8' OWNER ${POSTGRES_USER};
+EOSQL
